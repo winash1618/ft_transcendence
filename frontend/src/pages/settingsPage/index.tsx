40aa1ac67f2524a46400fd6709cd0edd5a52cdfa
Ahlam Salem <ahlamsalem@Ahlams-MacBook-Pro.local>
2023-04-09 22:38:22 +0400

    created setting page and added change nickname

--- frontend/src/pages/settingsPage/index.tsx
+++ frontend/src/pages/settingsPage/index.tsx
@@ -1,6 +1,55 @@
+import { useForm, SubmitHandler, Controller } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { PlaySchema } from "../../utils/schema";
+import { FormInput } from "../nickNamePage/nickNamePage.styled";
+import { useAppSelector } from "../../hooks/reduxHooks";
+import { FormContainer, FormDetails, FormInputTitle, FormTitle, InputAlert, InputController } from "./settingsPage.styled";
+import ButtonComponent from "../../components/ButtonComponent";
+
+export type SettingsType = {
+  nickname: string;
+};
 
 const SettingsPage = () => {
-  return <div>SettingsPage</div>;
+  const { userInfo } = useAppSelector((state) => state.auth);
+  const {
+    handleSubmit,
+    formState: { errors },
+    control,
+  } = useForm<SettingsType>({ resolver: yupResolver(PlaySchema) });
+
+  const onSubmit: SubmitHandler<SettingsType> = (data) => {};
+
+  return (
+    <FormContainer onSubmit={handleSubmit(onSubmit)}>
+      <FormTitle>Settings</FormTitle>
+      <FormDetails>
+        <InputController>
+          <FormInputTitle htmlFor="nickname">Change Nickname</FormInputTitle>
+          <Controller
+            control={control}
+            defaultValue={userInfo.username}
+            name="nickname"
+            render={({ field: { onChange, value } }) => (
+              <FormInput
+                onChange={onChange}
+                value={value}
+                placeholder="Enter a new nickname"
+                id="nickname"
+              />
+            )}
+          />
+          {errors.nickname && <InputAlert>{errors.nickname.message}</InputAlert>}
+        </InputController>
+        <ButtonComponent
+          style={{ width: "100%", marginBottom: "6px" }}
+          htmlType="submit"
+        >
+          Change nickname
+        </ButtonComponent>
+      </FormDetails>
+    </FormContainer>
+  );
 };
 
 export default SettingsPage;
---
+++ frontend/src/pages/settingsPage/settingsPage.styled.tsx
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+
+export const FormContainer = styled.form`
+  width: 100%;
+  height: 100%;
+  padding: 3rem 2rem;
+  background: var(--bg-gray);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const FormDetails = styled.div`
+  width: 100%;
+  max-width: 373px;
+`;
+
+export const InputController = styled.div`
+  margin-bottom: 2rem;
+`;
+
+export const FormInputTitle = styled.label`
+  display: block;
+  font-weight: 700;
+  font-size: 1rem;
+  line-height: 17px;
+  margin-bottom: 6px;
+`;
+
+export const FormTitle = styled.div`
+  text-align: center;
+  font-weight: 600;
+  font-size: 2.25rem;
+  line-height: 30px;
+  padding-right: 10px;
+  /* border-right: 1px solid #000; */
+  letter-spacing: 1px;
+  margin-bottom: 3rem;
+`;
+
+export const InputAlert = styled.p`
+  color: #cdcdcd;
+  padding-top: 6px;
+`;
