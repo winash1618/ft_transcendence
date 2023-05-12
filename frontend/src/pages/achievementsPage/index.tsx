d348fab11045651ff66e30b4027284d188cb80f1
kazemsami <kazemzidane@gmail.com>
2023-05-13 00:45:10 +0400

    done with achievements

--- frontend/src/pages/achievementsPage/index.tsx
+++ frontend/src/pages/achievementsPage/index.tsx
@@ -15,34 +15,54 @@ const AchievementsPage = () => {
   const { userInfo } = useAppSelector((state) => state.auth);
   const [achievements, setAchievements] = useState<any[]>([]);
   useEffect(() => {
-    const fetchAchievements = async () => {
-      const response = await axiosPrivate.get(
-        `/users/achievements/${userInfo.id}`
-      );
-      setAchievements([
-        {
-          id: 1,
-          title: "Da Player",
-          description: "Play your first game to unlock this achievement",
-          image: <FontAwesomeIcon color={response.data.played_first ? "yellow" : "black"} size="3x" icon={faTrophy} />,
-        },
-        {
-          id: 2,
-          title: "Overlord",
-          description: "Win 3 games to unlock this achievement",
-          image: <FontAwesomeIcon color={response.data.won_three ? "yellow" : "black"} size="3x" icon={faTrophy} />,
-        },
-        {
-          id: 3,
-          title: "Legendary",
-          description: "Win 10 games to unlock this achievement",
-          image: <FontAwesomeIcon color={response.data.won_ten ? "yellow" : "black"} size="3x" icon={faTrophy} />,
-        },
-      ]);
-      console.log(response.data);
-    };
-    fetchAchievements();
-  });
+    if (userInfo.id) {
+      const fetchAchievements = async () => {
+        const response = await axiosPrivate.get(
+          `/users/achievements/${userInfo.id}`
+        );
+        setAchievements([
+          {
+            id: 1,
+            title: "Da Player",
+            description: "Play your first game to unlock this achievement",
+            image: (
+              <FontAwesomeIcon
+                color={response.data.played_first ? "yellow" : "black"}
+                size="3x"
+                icon={faTrophy}
+              />
+            ),
+          },
+          {
+            id: 2,
+            title: "Overlord",
+            description: "Win 3 games to unlock this achievement",
+            image: (
+              <FontAwesomeIcon
+                color={response.data.won_three ? "yellow" : "black"}
+                size="3x"
+                icon={faTrophy}
+              />
+            ),
+          },
+          {
+            id: 3,
+            title: "Legendary",
+            description: "Win 10 games to unlock this achievement",
+            image: (
+              <FontAwesomeIcon
+                color={response.data.won_ten ? "yellow" : "black"}
+                size="3x"
+                icon={faTrophy}
+              />
+            ),
+          },
+        ]);
+        console.log(response.data);
+      };
+      fetchAchievements();
+    }
+  }, [userInfo]);
   return (
     <AchievementsContainer style={{ width: "100%" }}>
       <CustomRow style={{ paddingTop: "30px" }}>
